0b11fcbba41db49737ea033895b0fc490acaa8a4
bianhongxiang <bianhongxiang@bianhongxiangdeMacBook.local>
2020-09-28 00:29:05 +0800

    version002

--- week06/MyDjango/DouBan/models.py
+++ week06/MyDjango/DouBan/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# 图书or电影
+class t1(models.Model):
+    n_star = models.IntegerField()
+    short = models.CharField(max_length=400)
+    sentiment = models.FloatField()
