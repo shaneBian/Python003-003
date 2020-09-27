c5fc7d0ad58584e6e0e62b26b3e1f3949991667d
bianhongxiang <bianhongxiang@bianhongxiangdeMacBook.local>
2020-09-28 01:13:07 +0800

    version003

--- week06/MyDjango/DouBan/views.py
+++ week06/MyDjango/DouBan/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Hello Django!")
+from .models import t1
+
+def movies(request):
+    ###  从models取数据传给template  ###
+    #n = t1.objects.all()
+    #queryset = t1.objects.values('n_star')
+    #condtions = {'n_star__gt': 3}
+    #n = queryset.filter(**condtions).all()
+    n = t1.objects.exclude(n_star__lt=3).all()
+    return render(request, 'movies.html', locals())
